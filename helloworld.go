f269728ef61b08a0bccd5e2b152528d2848bb92b
liuweigang <liuweigang@topnet.net.cn>
2016-01-21 14:58:51 +0800

    sql and mongodb

---
+++ helloworld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "helloworld"
+
+func main() {
+	b := helloworld.SayHi(2)
+	fmt.Println(b)
+}
---
+++ helloworld/ptr.go
@@ -0,0 +1,5 @@
+package helloworld
+
+func SayHi(a int) (b int) {
+	return a + 1
+}
